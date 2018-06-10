9bc048824aeb036e035f41ccdf534f6b91f89f74
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-06-11 00:22:30 +0200

    demo.js can deploy contract and call functions on it

--- demo.js
+++ demo.js
@@ -2,63 +2,72 @@ const fs = require('fs');
 const solc = require('solc');
 const Web3 = require('web3');
 
-// Connect to local Ethereum node
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const provider = "http://localhost:8545";
 
-function deployContract(filename) {
-  // Compile the source code
-  const name = ":"+filename.replace(".sol", "");
-  const input = fs.readFileSync(filename);
-  const output = solc.compile(input.toString(), 1);
-  const bytecode = output.contracts[name].bytecode;
-  const abi = JSON.parse(output.contracts[name].interface);
+let web3;
 
-  // Contract object
-  const contract = web3.eth.contract(abi);
-
-  // Deploy contract instance
-  const contractInstance = contract.new({
-    data: '0x' + bytecode,
-    from: web3.eth.coinbase,
-    gas: 90000*2
-  }, (err, res) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    // Log the tx, you can explore status with eth.getTransaction()
-    console.log(res.transactionHash);
-
-  // If we have an address property, the contract was deployed
-  if (res.address) {
-    console.log('Contract address: ' + res.address);
-    // Let's test the deployed contract
-    testContract(res.address);
-  }
-  });
-
-  // Quick test the contract
-
-  function testContract(address) {
-    // Reference to the deployed contract
-    const token = contract.at(address);
-    // Destination account for test
-    const dest_account = '0x002D61B362ead60A632c0e6B43fCff4A7a259285';
-
-    // Assert initial account balance, should be 100000
-    const balance1 = token.balances.call(web3.eth.coinbase);
-    console.log(balance1 === 1000000);
-
-    // Call the transfer function
-    token.transfer(dest_account, 100, {from: web3.eth.coinbase}, (err, res) => {
-      // Log transaction, in case you want to explore
-      console.log('tx: ' + res);
-      // Assert destination account balance, should be 100
-      const balance2 = token.balances.call(dest_account);
-      console.log(balance2 === 100);
-    });
-  }
+/**
+ * Connects to the Ethereum provider specified in the variable "provider".
+ * @returns {Promise<Web3>} a Web3 instance.
+ */
+function connect() {
+  return new Promise((resolve, reject) => {
+    web3 = new Web3(new Web3.providers.HttpProvider(provider));
+    if (!web3.isConnected()) reject("Cannot connect to "+provider+".");
+    console.log("Connected to Web3: "+web3.version.node+".\n");
+    resolve(web3);
+  })
 }
 
-deployContract("CatalogContract.sol");
+/**
+ * Compile and deploy on Web3 a contract.
+ * @param filename the sol file to be deployed.
+ * @returns {Promise<web3.eth.contract>} the contract instance.
+ */
+function deployContract(filename) {
+  return new Promise((resolve, reject) => {
+    // Compile the source code
+    const name = ":"+filename.replace(".sol", "");
+    const input = fs.readFileSync(filename);
+    const output = solc.compile(input.toString(), 1);
+    const bytecode = output.contracts[name].bytecode;
+    const abi = JSON.parse(output.contracts[name].interface);
+
+    const contract = web3.eth.contract(abi);
+
+    const options = {
+      data: '0x' + bytecode,
+      from: web3.eth.coinbase,
+      gas: 4712388
+    };
+
+    // Deploy contract instance
+    const contractInstance = contract.new(options, (err, res) => {
+      if (err) reject(err);
+      if (res.address) {
+        console.log("Contract address: " + res.address);
+        resolve(contractInstance);
+      }
+    });
+  })
+}
+
+/**
+ * Main function of the program.
+ * @returns {Promise<void>} because the function is async.
+ */
+async function main() {
+  await connect();
+  const catalogContract = await deployContract('CatalogContract.sol');
+  console.log(catalogContract.contentCost());
+}
+
+
+// noinspection JSIgnoredPromiseFromCall
+main();
+
+/*main()
+  .catch(err => {
+    console.error("ERROR: "+err);
+    process.exit(1);
+  });*/
