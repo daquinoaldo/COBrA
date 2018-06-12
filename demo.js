1397b31ad165af7a8293da97b16db19c0d2185b8
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-06-12 16:14:13 +0200

    Catalog and GenericContents deployed successfully

--- demo.js
+++ demo.js
@@ -4,10 +4,11 @@ const Web3 = require('web3');
 
 const provider = "http://localhost:8545";
 const genres = ["adventure", "fantasy", "romance", "horror"];
-const contentsNum = 30;
+const contentsNumber = 1;
+const gas = 4712388;
 
 let web3;
-const contentContracts = [];
+let catalogContract;
 
 /**
  * Connects to the Ethereum provider specified in the variable "provider".
@@ -31,18 +32,23 @@ function connect() {
 function deployContract(filename = "Contract.sol", address = web3.eth.coinbase) {
   return new Promise((resolve, reject) => {
     // Compile the source code
-    const name = ":"+filename.replace(".sol", "");
+    const outputContractName = filename+":"+filename.replace(".sol", "");
     const input = fs.readFileSync(filename);
-    const output = solc.compile(input.toString(), 1);
-    const bytecode = output.contracts[name].bytecode;
-    const abi = JSON.parse(output.contracts[name].interface);
+    function findImports(path) {
+      return { contents: fs.readFileSync(path).toString() }
+    }
+    const source = { };
+    source[filename] = input.toString();
+    const output = solc.compile({ sources: source }, 1, findImports);
+    const bytecode = output.contracts[outputContractName].bytecode;
+    const abi = JSON.parse(output.contracts[outputContractName].interface);
 
     const contract = web3.eth.contract(abi);
 
     const options = {
       data: '0x' + bytecode,
       from: address,
-      gas: 4712388
+      gas: gas
     };
 
     // Deploy contract instance
@@ -65,11 +71,39 @@ function generateContents(num = 0) {
   const contents = [];
   for (let i = 0; i < num; i++)
     contents[i] = {
-      name: "title"+i,
-      content: "This is content "+i,
-      genre: genres[rand(0, genres.length-1)]
+      name: web3.fromAscii("title"+i),
+      content: web3.fromAscii("This is content "+i),
+      genre: web3.fromAscii(genres[rand(genres.length-1)])
     };
   return contents;
+}
+
+/**
+ * Auxiliary function: deploy num content contracts on the blockchain and returns it in an array.
+ * @param num the number of contents that you want to deploy.
+ * returns {Promise<contract[]>} an array contract instances.
+ */
+async function deployContentsContract(num) {
+  function getParams(owner = web3.eth.coinbase) { return { from: owner, gas: gas } }
+  // deploy num empty Contents
+  const promises = [];
+  const contentContracts = [];
+  for (let i = 0; i < num; i++)
+    promises[i] = deployContract('GenericContentManagementContract.sol',
+      web3.eth.accounts[rand(web3.eth.accounts.length)])
+      .then(contractInstance => contentContracts.push(contractInstance));
+  await Promise.all(promises);
+  // set the name, content and genre of each content
+  const contents = generateContents(num);
+  for (let i = 0; i < num; i++) {
+    const owner = contentContracts[i].author();
+    contentContracts[i].setName(contents[i].name, getParams(owner));
+    contentContracts[i].setContent(contents[i].content, getParams(owner));
+    contentContracts[i].setGenre(contents[i].genre, getParams(owner));
+    console.log(catalogContract.address);
+    contentContracts[i].publish(catalogContract.address, getParams(owner));
+  }
+  return contentContracts;
 }
 
 /**
@@ -91,21 +125,14 @@ function rand(to = 1, from = 0) {
 async function main() {
   await connect();
   // deploy the Catalog
-  const catalogContract = await deployContract('CatalogContract.sol');
-  // deploy contentsNum empty Contents
-  const promises = [];
-  for (let i = 0; i < contentsNum; i++)
-    promises[i] = deployContract('BaseContentManagementContract.sol')
-      .then(contractInstance => contentContracts.push(contractInstance));
-  await Promise.all(promises);
-  // set the name, content and genre of each content
-  const contents = generateContents(contentsNum);
-  for (let i = 0; i < contentsNum; i++) {
-    contentContracts[i].setName(contents[i].name);
-    contentContracts[i].setContent(contents[i].content);
-    contentContracts[i].setGenre(contents[i].genre);
-    contentContracts[i].publish(catalogContract.address);
-  }
+  console.log("Deploying catalog...");
+  catalogContract = await deployContract('CatalogContract.sol');
+  console.log("\nDeploying "+contentsNumber+" contents...");
+  const contentContracts = await deployContentsContract(contentsNumber);
+
+  // TEST
+  for (let i = 0; i < contentsNumber; i++)
+    console.log(contentContracts[i].name());
   //console.log(catalogContract.contentCost());
 }
 
