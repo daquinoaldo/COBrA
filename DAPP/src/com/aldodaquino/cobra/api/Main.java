e5190d2bfd820b006d2ef09c051ff5c6d999e41d
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-06-26 12:28:35 +0200

    API

---
+++ DAPP/src/com/aldodaquino/cobra/api/Main.java
@@ -0,0 +1,40 @@
+package com.aldodaquino.cobra.api;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+import com.aldodaquino.javautils.HttpRequestHelper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+/**
+ * HTTP Server for the REST API
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // create server
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        // set handlers
+        server.createContext("/test", new MyHandler());
+
+        // start server
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange request) throws IOException {
+            Map<String, Object> parameters = HttpRequestHelper.parseGET(request);
+
+            // send response
+            String response = "";
+            HttpRequestHelper.sendResponse(request, response);
+        }
+    }
+
+}
