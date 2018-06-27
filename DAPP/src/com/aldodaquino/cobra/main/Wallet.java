b5b93c004f1acb8706ac1092799ec1460bd985ee
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-06-27 15:47:01 +0200

    Converted in static function

--- DAPP/src/com/aldodaquino/cobra/main/Wallet.java
+++ DAPP/src/com/aldodaquino/cobra/main/Wallet.java
@@ -3,6 +3,7 @@ package com.aldodaquino.cobra.main;
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,9 +16,7 @@ public class Wallet {
     private static final String WALLET_PATH = "wallet";
     private static final String WALLET_PW = "password";
 
-    private Credentials credentials;
-
-    public Wallet() {
+    public static Credentials getOrCreateCredentials(Web3j web3) {
         try {
             // load the wallet folder
             File folder = new File(WALLET_PATH);
@@ -28,24 +27,21 @@ public class Wallet {
 
             String walletPath;
 
+            // create credentials if not exist
             if (files == null || files.length == 0) {  // no wallets
                 String fileName;
                 fileName = WalletUtils.generateFullNewWalletFile(WALLET_PW, folder);
                 walletPath = WALLET_PATH + "/" + fileName;
-                System.out.println("com.aldodaquino.cobra.main.Wallet created in " + walletPath);
+                System.out.println("Wallet created in " + walletPath);
             }
             else walletPath = files[0].getAbsolutePath();
 
-            // load credentials
-            credentials = WalletUtils.loadCredentials(WALLET_PW, walletPath);
+            // load and return credentials
+            return WalletUtils.loadCredentials(WALLET_PW, walletPath);
         } catch (IOException | NoSuchAlgorithmException | CipherException | NoSuchProviderException |
                 InvalidAlgorithmParameterException e) {
             e.printStackTrace();
             throw new RuntimeException("Cannot load or create credentials.");
         }
-    }
-
-    public Credentials getCredentials() {
-        return credentials;
     }
 }
