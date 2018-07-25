e09eef88f50df7419be8bb7f49f6f42a42905de9
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-07-25 10:06:32 +0200

    Removed trash

--- trash/SocialGossip/gui/constants/Icons.java
+++
@@ -1,7 +0,0 @@
-package gui.constants;
-
-import javax.swing.*;
-
-public class Icons {
-    public static final ImageIcon logo = new ImageIcon(Icons.class.getResource("/gui/media/logo.png"));
-}
--- trash/website/builder/api/get-username.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "functions.php";
-
-$username = getUsername();
-if(empty($username)) die(newMessage(-1, "Not logged in."));
-
-echo newMessage(0, $username);
